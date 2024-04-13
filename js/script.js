b616fff02369445e0f018ea9ffb092fb5b66a9ac
vanmukilan.R VANMUKILAN.R <142682590+vanmukilan@users.noreply.github.com>
2024-04-13 20:12:28 +0530

    Update script.js

--- js/script.js
+++ js/script.js
@@ -26,9 +26,37 @@ window.onscroll = () => {
            })
         };
     })
-  
+    /*==================== sticky navbar ====================*/
+
+    let header = document.querySelector('header');
+
+    header.classList.toggle('sticky',window.scrollY > 100);
+
+    /*==================== remove toggle icon and navbar when click navbar link (scroll) ====================*/
+
+    menuIcon.classList.remove('bx-x')
+    navbar.classList.remove('active')
+};
 
 
-    
+/*==================== scroll reveal ====================*/
+ScrollReveal({ 
+    // reset: true,
+    distance:'80px',
+    duration:2000,
+    delay:200
+ });
 
+ ScrollReveal().reveal('.home-content, .heading', {origin:'top'});
+ ScrollReveal().reveal('.home-img, .services-container, .portfolio, .contact form', {origin:'bottom'});
+ ScrollReveal().reveal('.home-content h1, .about-img', {origin:'left'});
+ ScrollReveal().reveal('.home-content p, .about-content', {origin:'right'});
 
+/*==================== typed js ====================*/
+const typed = new Typed('.multiple-text',{
+     strings: ['Frontend Developer'],
+     typeSpeed:100,
+     backSpeed:100,
+     backdelay:1000,
+     loop:true
+});
