b3d75fe1c1025e792316a04d0e083ea192a5b300
vanmukilan.R VANMUKILAN.R <142682590+vanmukilan@users.noreply.github.com>
2024-04-13 20:04:16 +0530

    Update script.js

--- js/script.js
+++ js/script.js
@@ -1,16 +1,6 @@
 
 
-    /*==================== sticky navbar ====================*/
-
-    let header = document.querySelector('header');
-
-    header.classList.toggle('sticky',window.scrollY > 100);
-
-    /*==================== remove toggle icon and navbar when click navbar link (scroll) ====================*/
-
-    menuIcon.classList.remove('bx-x')
-    navbar.classList.remove('active')
-};
+    
 
 
 /*==================== scroll reveal ====================*/
