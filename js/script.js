c4798950a6bb1a26af0fc03a92a0a53acf7de82f
vanmukilan.R VANMUKILAN.R <142682590+vanmukilan@users.noreply.github.com>
2024-04-13 20:00:32 +0530

    Update script.js

--- js/script.js
+++ js/script.js
@@ -1,11 +1,3 @@
-/*==================== toggle icon navbar ====================*/
-let menuIcon = document.querySelector('#menu-icon');
-let navbar = document.querySelector('.navbar');
-
-menuIcon.onclick = () => {
-    menuIcon.classList.toggle('bx-x');
-    navbar.classList.toggle('active');
-};
 
 
 /*==================== scroll sections active link ====================*/
