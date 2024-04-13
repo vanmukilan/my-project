56a30425fb2934cac42c7f03f9dad0cabc2deb2d
vanmukilan.R VANMUKILAN.R <142682590+vanmukilan@users.noreply.github.com>
2024-04-13 19:57:10 +0530

    Update script.js

--- js/script.js
+++ js/script.js
@@ -6,6 +6,8 @@ menuIcon.onclick = () => {
     menuIcon.classList.toggle('bx-x');
     navbar.classList.toggle('active');
 };
+
+
 /*==================== scroll sections active link ====================*/
 let sections = document.querySelectorAll('section');
 let navLinks = document.querySelectorAll('header nav a');
@@ -35,6 +37,8 @@ window.onscroll = () => {
     menuIcon.classList.remove('bx-x')
     navbar.classList.remove('active')
 };
+
+
 /*==================== scroll reveal ====================*/
 ScrollReveal({ 
     // reset: true,
@@ -48,6 +52,11 @@ ScrollReveal({
  ScrollReveal().reveal('.home-content h1, .about-img', {origin:'left'});
  ScrollReveal().reveal('.home-content p, .about-content', {origin:'right'});
 
-
-
-
+/*==================== typed js ====================*/
+const typed = new Typed('.multiple-text',{
+     strings: ['Frontend Developer'],
+     typeSpeed:100,
+     backSpeed:100,
+     backdelay:1000,
+     loop:true
+});
